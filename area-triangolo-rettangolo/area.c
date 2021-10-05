705abfef1a7f2ef88de1230c3e30874c2ab4bf37
Tommaso Benatti <tommaso.benatti@outlook.com>
2021-10-05 11:24:11 +0200

    aggiunta commenti

--- area-triangolo-rettangolo/area.c
+++ area-triangolo-rettangolo/area.c
@@ -8,20 +8,24 @@ int main(){
 	int nArea_rettangolo;
 	float fArea_triangolo;
 	
+// Titolo
 	printf("Calcolatore area rettangolo & triangolo - Tommaso Benatti 3T 05/10/2021 Project nr.2 - ITI Copernico Ferrara\n\n");
 
-	
+// Chiedi all'utente i dati	
 	printf("Inserisci la base:\n> ");
 	scanf("%d",&nBase);
 	printf("Inserisci l'altezza:\n> ");
 	scanf("%d",&nAltezza);
-	
+
+// Calcola i dati	
 	nArea_rettangolo = nBase * nAltezza;
 	fArea_triangolo = (float) (nBase * nAltezza) / 2;
-	
+
+// Mostra i dati all'utente	
 	printf("\n> L'area del rettangolo e': %d", nArea_rettangolo);
 	printf("\n> L'area del triangolo e': %f", fArea_triangolo);
 	
+// Controllo errori
 	return 0;
 	
 }
