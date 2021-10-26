5d20bf5ba9c52732c8414c0e31f04496a8197457
Tommaso Benatti <tommaso.benatti@outlook.com>
2021-10-26 09:44:02 +0200

    upodate

---
+++ verifica 1/verifica.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+// Codice disponibile al link https://github.com/tommasobenatti/c-scuola
+
+int main(){
+	
+	int nNumero;
+	
+// Titolo
+	printf("Verifica 1 - Tommaso Benatti 3T 26/10/2021 Project nr.1 - ITI Copernico Ferrara\n\n");
