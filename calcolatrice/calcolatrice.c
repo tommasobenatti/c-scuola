5e050c4c0d649b05ce2328099a2de22d290e6ba0
Tommaso Benatti <tommaso.benatti@outlook.com>
2021-11-16 11:53:34 +0100

    okkkk

--- calcolatrice/calcolatrice.c
+++ calcolatrice/calcolatrice.c
@@ -6,18 +6,17 @@
 int main()
 {
 // Dichiarazione variabili
-    int nNum1, nNum2;
+    float fNum1, fNum2, fRisultato;
     char cOperazione;
-    float fRisultato;
 
 // Titolo
 	printf("Calcolatrice - Tommaso Benatti 3T 16/11/2021 - ITI Copernico Ferrara\n\n");
 
 // Richiesta dati all'utente
     printf("Inserisci il primo numero\n> ");
-    scanf("%d", &nNum1);
+    scanf("%f", &fNum1);
     printf("\nInserisci il secondo numero\n> ");
-    scanf("%d", &nNum2);
+    scanf("%f", &fNum2);
     printf("\nInserisci l'operazione da eseguire (+, -, *, /)\n> ");
     fflush(stdin); // Pulisce lo stdin per inserire il char
     scanf("%c", &cOperazione);
@@ -26,25 +25,25 @@ int main()
     switch (cOperazione)
     {
         case '+': // Addizione
-            fRisultato = nNum1 + nNum2;
+            fRisultato = fNum1 + fNum2;
             printf("Il risultato dell'addizione è: %f", fRisultato);
             break;
         case '-': // Sottrazione
-            fRisultato = nNum1 - nNum2;
+            fRisultato = fNum1 - fNum2;
             printf("Il risultato della sottrazione è: %f", fRisultato);
             break;
         case '*': // Moltiplicazione
-            fRisultato = nNum1 * nNum2;
+            fRisultato = fNum1 * fNum2;
             printf("Il risultato della moltiplicazione è: %f", fRisultato);
             break;
         case '/': // Divisione
-            if(nNum2 == 0)
+            if(fNum2 == 0)
             {
                 printf("Errore: divisione per zero"); // Ha diviso per zero
             }
             else
             {
-                fRisultato = nNum1 / nNum2;
+                fRisultato = fNum1 / fNum2;
                 printf("Il risultato della divisione è: %f", fRisultato); // Ha diviso per un numero diverso da zero
             }
             break;
